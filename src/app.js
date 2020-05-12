bbecb0a6e00c0e9b4613f3c9e3e66b05345bca21
Alejandro <836445882@qq.com>
2020-05-12 15:34:36 +0800

    添加input测试用例； 以及让input支持v-model双向绑定

--- src/app.js
+++ src/app.js
@@ -14,88 +14,13 @@ new Vue({
     data: {
         loading1: false,
         loading2: true,
-        loading3: false
+        loading3: false,
+        message: 'hi'
+    },
+    methods: {
+        inputChange(e) {
+            console.log(e.target.value);
+        }
     }
 
 })
-
-
-// import chai from 'chai'
-
-// import spies from 'chai-spies'
-
-// chai.use(spies)
-
-// const expect = chai.expect
-// // 单元测试
-
-// {
-//     // 测试按钮含有 icon
-//     const Constructor = Vue.extend(Button)
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: 'settings'
-//         }
-//     }).$mount()
-//     const useElement = vm.$el.querySelector('use')
-//     expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
-//     vm.$destroy()
-// }
-// {
-//     const Constructor = Vue.extend(Button)
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: 'settings',
-//             loading: true
-//         }
-//     }).$mount()
-//     const useElements = vm.$el.querySelectorAll('use')
-//     expect(useElements.length).to.equal(1)
-//     expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
-//     vm.$destroy()
-// }
-// {
-//     const div = document.createElement('div')
-//     document.body.appendChild(div)
-//     const Constructor = Vue.extend(Button)
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: 'settings',
-//         }
-//     }).$mount(div)
-//     const icon = vm.$el.querySelector('svg')
-//     expect(getComputedStyle(icon).order).to.eq('1')
-//     vm.$el.remove()
-//     vm.$destroy()
-// }
-// {
-//     const div = document.createElement('div')
-//     document.body.appendChild(div)
-//     const Constructor = Vue.extend(Button)
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: 'settings',
-//             iconPosition: 'right'
-//         }
-//     }).$mount(div)
-//     const icon = vm.$el.querySelector('svg')
-//     expect(getComputedStyle(icon).order).to.eq('2')
-//     vm.$el.remove()
-//     vm.$destroy()
-// }
-// {
-//     const Constructor = Vue.extend(Button)
-//     const vm = new Constructor({
-//         propsData: {
-//             icon: 'settings',
-//         }
-//     }).$mount()
-//     // vm.$on('click', function(){
-//     //   console.log('hi')
-//     // })
-//     // vm.$el.click()
-//     const spy = chai.spy(() => { })
-//     vm.$on('click', spy)
-//     vm.$el.click()
-//     expect(spy).to.have.been.called()
-// }
