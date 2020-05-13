af420ecf0109fc813114faef5d37228b1ecc03f7
Alejandro <836445882@qq.com>
2020-05-13 11:06:32 +0800

    添加 间隔

--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="row" >
+  <div
+    class="row"
+    :style="{ marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px' }"
+  >
     <slot></slot>
   </div>
 </template>
@@ -11,10 +14,21 @@ export default {
       type: [Number, String],
     },
   },
+  created() {
+    console.log('row created');
+    //   没有儿子
+  },
+  mounted() {
+    console.log('row mounted');
+    console.log(this.$children);
+    this.$children.forEach((vm)=>{
+        vm.gutter = this.gutter
+    })
+  },
 };
 </script>
 <style lang="scss" scoped>
-.row{
-    display: flex;
+.row {
+  display: flex;
 }
 </style>
