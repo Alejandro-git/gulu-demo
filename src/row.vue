f316d20a4cf0472b9b5d68f247b83824e150aee5
Alejandro <836445882@qq.com>
2020-05-13 10:16:25 +0800

    row 和 col 的雏形完成

---
+++ src/row.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row" >
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "LunziRow",
+  props: {
+    gutter: {
+      type: [Number, String],
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.row{
+    display: flex;
+}
+</style>
