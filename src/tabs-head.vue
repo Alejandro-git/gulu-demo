1cd17bd9bd99accf2202a7dafda803164ca4eecb
Alejandro <836445882@qq.com>
2020-05-15 15:08:38 +0800

    增大 tabs 的 item 的可点击区域

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -23,7 +23,6 @@ $blue: blue;
   display: flex;
   height: $tab-height;
   justify-content: flex-start;
-  align-items: center;
   border-bottom: 1px solid red;
   > .actions-wrapper {
     margin-left: auto;
