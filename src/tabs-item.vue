41e3508a2eef679b6d2fc8fc45f6f90c0f0801a2
Alejandro <836445882@qq.com>
2020-05-15 10:17:04 +0800

    添加 tabs 相关组件，添加基本props

---
+++ src/tabs-item.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tabs-item">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LunziTabsItem",
+  props: {
+    disable: {
+      type: Boolean,
+      default: true,
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.tabs-item {
+}
+</style>
