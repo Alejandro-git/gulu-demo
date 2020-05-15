4b839a03d406257a4b4aa7738524b502c49054d9
Alejandro <836445882@qq.com>
2020-05-15 17:29:32 +0800

    添加 tabs 部分（tabs和tabs-item）测试用例

--- src/tabs.vue
+++ src/tabs.vue
@@ -38,6 +38,11 @@ export default {
     //   this.$emit('update:selected', 'xxx')
   },
   mounted() {
+    if (this.$children.length === 0) {
+      console &&
+        console.warn &&
+        console.warn("tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件");
+    }
     this.$children.forEach((vm) => {
       if (vm.$options.name === "LunziTabsHead") {
         vm.$children.forEach((childVm) => {
