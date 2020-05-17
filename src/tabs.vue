40f557f1049139dbd62ca9403c181b8394838887
Alejandro <836445882@qq.com>
2020-05-17 19:27:59 +0800

    重构 tsbs.vue 组件

--- src/tabs.vue
+++ src/tabs.vue
@@ -37,24 +37,34 @@ export default {
   created() {
     //   this.$emit('update:selected', 'xxx')
   },
-  mounted() {
-    if (this.$children.length === 0) {
-      console &&
-        console.warn &&
-        console.warn("tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件");
-    }
-    this.$children.forEach((vm) => {
-      if (vm.$options.name === "LunziTabsHead") {
-        vm.$children.forEach((childVm) => {
-          if (
-            childVm.$options.name === "LunziTabsItem" &&
-            childVm.name === this.selected
-          ) {
-            this.eventBus.$emit("update:selected", this.selected, childVm);
-          }
-        });
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console &&
+          console.warn &&
+          console.warn(
+            "tabs的子组件应该是tabs-head和tabs-nav，但你没有写子组件"
+          );
       }
-    });
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name === "GuluTabsHead") {
+          vm.$children.forEach((childVm) => {
+            if (
+              childVm.$options.name === "GuluTabsItem" &&
+              childVm.name === this.selected
+            ) {
+              this.eventBus.$emit("update:selected", this.selected, childVm);
+            }
+          });
+        }
+      });
+    },
+  },
+  mounted() {
+    this.checkChildren();
+    this.selectTab();
   },
 };
 </script>
